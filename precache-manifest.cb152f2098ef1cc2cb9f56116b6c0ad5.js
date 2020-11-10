4b0f5c43b1267546574b440f1dd09a00b4cbaf70
Sanmi01 <supersanmi01@gmail.com>
2020-11-10 22:16:15 +0100

    Updates

---
+++ precache-manifest.cb152f2098ef1cc2cb9f56116b6c0ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ceba223e4c6546455a2f325c6d30a1",
+    "url": "/react-app-shop/index.html"
+  },
+  {
+    "revision": "d287203ddb5b98c90539",
+    "url": "/react-app-shop/static/css/main.b01a0161.chunk.css"
+  },
+  {
+    "revision": "0373682d14ddf33e90cb",
+    "url": "/react-app-shop/static/js/2.555ae1a8.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react-app-shop/static/js/2.555ae1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d287203ddb5b98c90539",
+    "url": "/react-app-shop/static/js/main.6462cf31.chunk.js"
+  },
+  {
+    "revision": "2909a2690bce2715117a",
+    "url": "/react-app-shop/static/js/runtime-main.b983947c.js"
+  }
+]);
